00c7872cf768984385323c8cf83121c8d471824d
Younes Nejahi <younes@wayne.edu>
2020-05-13 10:19:02 -0700

    fixed coeff

--- src/moves/MultiParticle.h
+++ src/moves/MultiParticle.h
@@ -324,11 +324,11 @@ inline void MultiParticle::Accept(const uint rejectState, const uint step)
 {
   // Here we compare the values of reference and trial and decide whether to
   // accept or reject the move
-  long double MPCoeff = GetCoeff();
+  long double MPCoeff = 1.3050e+04;//GetCoeff();
   double uBoltz = exp(-BETA * (sysPotNew.Total() - sysPotRef.Total()));
   long double accept = MPCoeff * uBoltz;
-  cout << "MPCoeff: " << MPCoeff << ", sysPotNew: " << sysPotNew.Total()
-       << ", sysPotRef: " << sysPotRef.Total() << ", accept: " << accept <<endl;
+  // cout << "MPCoeff: " << MPCoeff << ", sysPotNew: " << sysPotNew.Total()
+  //      << ", sysPotRef: " << sysPotRef.Total() << ", accept: " << accept <<endl;
   bool result = (rejectState == mv::fail_state::NO_FAIL) && prng() < accept;
   if(result) {
     sysPotRef = sysPotNew;
