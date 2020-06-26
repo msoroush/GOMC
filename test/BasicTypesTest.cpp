bd8ec7ab9add58f6d32f1eeeac2aa22b7d8a670b
Younes Nejahi <younes@wayne.edu>
2020-06-25 23:00:16 -0700

    more unit tests added

--- test/BasicTypesTest.cpp
+++ test/BasicTypesTest.cpp
@@ -22,3 +22,16 @@ TEST(BasicTypesTest, EqualOperatorTest) {
   EXPECT_EQ(point2.y, 99.012);
   EXPECT_EQ(point2.z, -991.594);
 }
+
+TEST(BasicTypesTest, NotEqualOperatorTest) {
+  XYZ point1(1.0, 231.2014, 120.18);
+  XYZ point2(1.0, 231.2014, 120.18);
+  XYZ point3(1.3, 9123.12, 12.18);
+  XYZ point4(1.3, 9123.120, 12.18);
+  XYZ point5(1.3, 9123.1201, 12.18);
+
+  EXPECT_FALSE(point1 != point2);
+  EXPECT_TRUE(point1 != point3);
+  EXPECT_FALSE(point3 != point4);
+  EXPECT_TRUE(point3 != point5);
+}
